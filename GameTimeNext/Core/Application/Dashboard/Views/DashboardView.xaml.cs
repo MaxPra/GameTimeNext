e5a2fb1ed2baeefb4e01303bddb0a7b4264bf365
MaxPra <epiccmax@gmx.at>
2026-06-13 10:36:15 +0200

    Dashboard (in progress) & update changes

---
+++ GameTimeNext/Core/Application/Dashboard/Views/DashboardView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UIX.ViewController.Engine.FrameworkElements.UserControls;
+
+namespace GameTimeNext.Core.Application.Dashboard.Views
+{
+    /// <summary>
+    /// Interaction logic for DashboardView.xaml
+    /// </summary>
+    public partial class DashboardView : UIXUserControlBase
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
