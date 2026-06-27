aed1f7510e61f94a9666b619ea1afd2d146878bb
MaxPra <epiccmax@gmx.at>
2026-06-27 19:13:35 +0200

    refactoring

--- GameTimeNext.Shared/SocketCom/ProfilesCom/ProfileCom.cs
+++ GameTimeNext.Shared/SocketCom/ProfilesCom/ProfileCom.cs
@@ -2,7 +2,7 @@
 {
     public class ProfileCom
     {
-        public bool Playable { get; set; }
+        public string State { get; set; } = ProfileStates.All;
         public bool Launchable { get; set; } = false;
         public string ProfileName { get; set; } = string.Empty;
         public string PicPath { get; set; } = string.Empty;
--- GameTimeNext.Shared/SocketCom/ProfilesCom/ProfileStates.cs
+++ GameTimeNext.Shared/SocketCom/ProfilesCom/ProfileStates.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameTimeNext.Shared.SocketCom.ProfilesCom
 {
-    public class PlayableStates
+    public class ProfileStates
     {
         public const string Playable = "Playable";
         public const string All = "All";
