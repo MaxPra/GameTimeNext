54c216c74f707ad0f1594c114e58a46311859ab2
MaxPra <epiccmax@gmx.at>
2026-06-27 19:12:29 +0200

    added playable states class

---
+++ GameTimeNext.Shared/SocketCom/ProfilesCom/PlayableStates.cs
@@ -0,0 +1,8 @@
+﻿namespace GameTimeNext.Shared.SocketCom.ProfilesCom
+{
+    public class PlayableStates
+    {
+        public const string Playable = "Playable";
+        public const string All = "All";
+    }
+}
